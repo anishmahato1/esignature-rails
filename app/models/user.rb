815daa7f8ab8b6f7c250f267f62729bb7d9e823c
anish <anishkumar.mahato@angelswing.io>
2025-05-05 09:48:24 +0545

    feat: format email for spaces and set default name for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,28 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :documents, dependent: :destroy
+  has_many :signatures, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
   validates :password, length: { minimum: 8 }
+
+  before_validation :squish_name_and_email_spaces!, :set_default_name
+
+  private
+
+  #
+  # Strips white spaces at ends and any extras in between
+  #
+  def squish_name_and_email_spaces!
+    [name, email].each { |attr| attr&.squish! }
+  end
+
+  #
+  # Sets the name to the email if it's not set
+  #
+  def set_default_name
+    self.name ||= email.split('@').first
+  end
 end
